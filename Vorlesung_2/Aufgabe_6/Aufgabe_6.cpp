bc81f2652d374ef389209cc495529192550871c3
Christian Schmiedt <120606421+cschm1ed@users.noreply.github.com>
2024-05-07 20:52:15 +0200

    V2 A6 done

--- Vorlesung_2/Aufgabe_6/Aufgabe_6.cpp
+++ Vorlesung_2/Aufgabe_6/Aufgabe_6.cpp
@@ -21,27 +21,33 @@ void fillMatrixRandom(double **matrix, int rows, int cols) {
 }
 
 void printMatrix(double **matrix, int rows, int cols) {
-	std::cout << "{";
+	std::cout << "\n";
 	for (int i = 0; i < rows; ++i) {
 		std::cout << "[";
-		for (int j = 0; j < rows; ++j) {
-			std::cout << matrix[i][j] << "";
+		for (int j = 0; j < cols; ++j) {
+			std::cout << matrix[i][j];
+			if (j < cols - 1)
+				std::cout << " ";
 		}
 		std::cout << "]\n";
 	}
+	std::cout << "\n";
 }
 
 double **multiply2dMatrix(double **m1, double **m2, std::pair<int, int> dims1, std::pair<int, int> dims2) {
-	double **resulting = alloc2dArray(dims2.second, dims1.first);
+	double **resulting = alloc2dArray(dims1.first, dims2.second);
 	double sum;
 
 	if (dims1.second != dims2.first) {
-		return NULL ;
+		return nullptr ;
 	}
-	for (int i = 0; i < dims2.second; ++i) {
-		sum = 0;
-		for (int j = 0; j < dims1.first; ++j) {
-
+	for (int i = 0; i < dims1.first; ++i) {
+		for (int j = 0; j < dims2.second; ++j) {
+			sum = 0;
+			for (int k = 0; k < dims1.second; k++) {
+				sum += m1[i][k] * m2[k][j];
+			}
+			resulting[i][j] = sum;
 		}
 	}
 	return resulting;
@@ -51,10 +57,19 @@ int main() {
 	const int rowsM1 = 3, columnsM1 = 4, columnsM2 = 2;
 	double **m1, **m2, **m3;
 
-	std::srand(std::time(NULL));
+	std::srand(std::time(nullptr));
 	m1 = alloc2dArray(rowsM1, columnsM1);
 	m2 = alloc2dArray(columnsM1, columnsM2);
 	fillMatrixRandom(m1, rowsM1, columnsM1);
 	fillMatrixRandom(m2, columnsM1, columnsM2);
-
+	std::cout << "Matrix1: ";
+	printMatrix(m1, rowsM1, columnsM1);
+	std::cout << "Matrix2: ";
+	printMatrix(m2, columnsM1, columnsM2);
+	m3 = multiply2dMatrix(m1, m2, std::pair<int, int> (rowsM1, columnsM1), std::pair<int, int> (columnsM1, columnsM2));
+	if (m3 == nullptr)
+		return 1;
+	std::cout << "Ergebnis: ";
+	printMatrix(m3, rowsM1, columnsM2);
+	return 0;
 }
